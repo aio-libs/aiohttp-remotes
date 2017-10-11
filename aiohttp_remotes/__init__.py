6e7615efffa8ce515bf5da39f8da3485ed70fe7f
Andrew Svetlov <andrew.svetlov@gmail.com>
2017-10-11 11:37:29 +0300

    Add initial flit file

--- aiohttp_remotes/__init__.py
+++ aiohttp_remotes/__init__.py
@@ -1,1 +1,9 @@
+"""Control remote side information.
+
+Properly sets up host, scheme and remote properties of
+aiohttp.web.Request if the server is deployed behind reverse proxy.
+
+"""
+
+
 __version__ = '0.0.1'
