d7153c790be0d267b86b04b3a8be76c41fd113b4
Andrew Svetlov <andrew.svetlov@gmail.com>
2017-10-25 17:23:30 +0300

    Test XForwardedRelaxed

--- aiohttp_remotes/x_forwarded.py
+++ aiohttp_remotes/x_forwarded.py
@@ -5,6 +5,7 @@ from aiohttp import hdrs, web
 from .abc import ABC
 from .exceptions import TooManyHeaders
 from .log import logger
+from .utils import parse_trusted_list
 
 
 class XForwardedRelaxed(ABC):
@@ -13,12 +14,12 @@ class XForwardedRelaxed(ABC):
         app.middlewares.append(self.middleware)
 
     def get_forwarded_for(self, headers):
-        forwarded_for = headers.getall(hdrs.X_FORWARDED_FOR)
+        forwarded_for = headers.getall(hdrs.X_FORWARDED_FOR, [])
         if not forwarded_for:
             return []
         if len(forwarded_for) > 1:
             raise TooManyHeaders(hdrs.X_FORWARDED_FOR)
-        forwarded_for = forwarded_for.split(',')
+        forwarded_for = forwarded_for[0].split(',')
         forwarded_for = [
             ip_address(addr) for addr in
             (a.strip() for a in forwarded_for)
@@ -28,102 +29,73 @@ class XForwardedRelaxed(ABC):
         return forwarded_for
 
     def get_forwarded_proto(self, headers):
-        forwarded_proto = headers.getall(hdrs.X_FORWARDED_PROTO)
+        forwarded_proto = headers.getall(hdrs.X_FORWARDED_PROTO, [])
         if not forwarded_proto:
             return []
         if len(forwarded_proto) > 1:
             raise TooManyHeaders(hdrs.X_FORWARDED_PROTO)
-        forwarded_proto = forwarded_proto.split(',')
+        forwarded_proto = forwarded_proto[0].split(',')
         forwarded_proto = [p.strip() for p in forwarded_proto]
 
         return forwarded_proto
 
     def get_forwarded_host(self, headers):
-        forwarded_host = headers.get(hdrs.X_FORWARDED_HOST, '')
+        forwarded_host = headers.getall(hdrs.X_FORWARDED_HOST, [])
         if len(forwarded_host) > 1:
             raise TooManyHeaders(hdrs.X_FORWARDED_HOST)
-        return forwarded_host
-
-    async def handler(self, request, handler):
-        overrides = {}
-
-        forwarded_for = self.get_forwarded_for()
-        if forwarded_for:
-            overrides['remote'] = str(forwarded_for[0])
-
-        proto = self.get_forwarded_proto()
-        if proto:
-            overrides['scheme'] = proto[0]
-
-        host = self.get_forwarded_host()
-        if host is not None:
-            overrides['host'] = host
-
-        request = request.clone(**overrides)
-
-        return await handler(request)
+        return forwarded_host[0] if forwarded_host else None
 
     @web.middleware
     async def middleware(self, request, handler):
         try:
-            return await self.handler(request, handler)
+            overrides = {}
+
+            headers = request.headers
+            forwarded_for = self.get_forwarded_for(headers)
+            if forwarded_for:
+                overrides['remote'] = str(forwarded_for[0])
+
+            proto = self.get_forwarded_proto(headers)
+            if proto:
+                overrides['scheme'] = proto[0]
+
+            host = self.get_forwarded_host(headers)
+            if host is not None:
+                overrides['host'] = host
+
+            request = request.clone(**overrides)
+
+            return await handler(request)
         except TooManyHeaders as exc:
             msg = 'Too many headers for %(header)s'
             context = {'header': exc.header}
             logger.error(msg, context, extra={'request': request,
                                               'header': exc.header})
-            self.raise_error(request)
+            await self.raise_error(request)
 
 
 class XForwardedStrict(XForwardedRelaxed):
 
-    def __init__(self, num_proxies=1, upstreams=(), *, white_paths=()):
-        self._num_proxies = num_proxies
-        self._upstreams = set(ip_network(i) for i in upstreams)
+    def __init__(self, trusted, *, white_paths=()):
+        self._trusted = parse_trusted_list(trusted)
         self._white_paths = set(white_paths)
 
-    def get_remote_addr(self, request):
-        forwarded_for = self._get_forwarded_for(request.headers)
-
-        trusted_header = len(forwarded_for) >= self._num_proxies
-
-        real_remote_addr, _ = request.transport.get_extra_info('peername')
-
-        if trusted_header:
-            trusted = False
-            _real_remote_addr = ip_address(real_remote_addr)
-
-            for upstream in self.upstreams:
-                if _real_remote_addr in upstream:
-                    trusted = True
-                    break
-
-            if trusted:
-                remote_addr = forwarded_for[-self.num_proxies]
-            else:
-                remote_addr = real_remote_addr
-        else:
-            remote_addr = real_remote_addr
-            trusted = False
-
-        return remote_addr, trusted
-
-    async def handler(self, request, handler):
-        if request.path not in self._white_paths:
+    @web.middleware
+    async def middleware(self, request, handler):
+        if request.path in self._white_paths:
+            return await handler(request)
+        try:
             overrides = {}
-            remote_addr, trusted = self.get_remote_addr(request)
 
-            if not trusted:
-                msg = 'Reverse proxy not found: %(remote_addr)s'
-                context = {'remote_addr': remote_addr}
-                logger.error(msg, context, extra={'request': request})
-
-                self.raise_error(request)
-            overrides['remote'] = str(remote_addr)
+            forwarded_for = self.get_forwarded_for()
+            peer_ip, _ = request.transport.get_extra_info('peername')
+            ips = [peer_ip] + list(reversed(forwarded_for))
+            remote_ip = remote_ip(self._trusted, ips)
+            overrides['remote'] = str(remote_ip)
 
             proto = self.get_forwarded_proto()
-            if proto is not None:
-                overrides['scheme'] = proto
+            if proto:
+                overrides['scheme'] = proto[0]
 
             host = self.get_forwarded_host()
             if host is not None:
@@ -131,4 +103,10 @@ class XForwardedStrict(XForwardedRelaxed):
 
             request = request.clone(**overrides)
 
-        return await handler(request)
+            return await handler(request)
+        except TooManyHeaders as exc:
+            msg = 'Too many headers for %(header)s'
+            context = {'header': exc.header}
+            logger.error(msg, context, extra={'request': request,
+                                              'header': exc.header})
+            await self.raise_error(request)
--- tests/test_x_forwarded.py
+++ tests/test_x_forwarded.py
@@ -1,17 +1,87 @@
 import aiohttp
 from aiohttp import web
 
-from aiohttp_remotes import XForwardedRelaxed
+from aiohttp_remotes import setup as _setup, XForwardedRelaxed
 
 
-async def xtest_x_forwarded_ok(test_client):
+async def test_x_forwarded_relaxed_ok(test_client):
+    async def handler(request):
+        assert request.host == 'example.com'
+        assert request.scheme == 'https'
+        assert request.secure
+        assert request.remote == '10.10.10.10'
+
+        return web.Response()
+
+    app = web.Application()
+    app.router.add_get('/', handler)
+    _setup(app, XForwardedRelaxed())
+    cl = await test_client(app)
+    resp = await cl.get('/', headers={'X-Forwarded-For': '10.10.10.10',
+                                      'X-Forwarded-Proto': 'https',
+                                      'X-Forwarded-Host': 'example.com'})
+    assert resp.status == 200
+
+
+async def test_x_forwarded_relaxed_no_forwards(test_client):
+    async def handler(request):
+        url = cl.make_url('/')
+        host = url.host + ':' + str(url.port)
+        assert request.host == host
+        assert request.scheme == 'http'
+        assert not request.secure
+        assert request.remote == '127.0.0.1'
+
+        return web.Response()
+
+    app = web.Application()
+    app.router.add_get('/', handler)
+    _setup(app, XForwardedRelaxed())
+    cl = await test_client(app)
+    resp = await cl.get('/')
+    assert resp.status == 200
+
+
+async def test_x_forwarded_relaxed_multiple_for(test_client):
     async def handler(request):
         return web.Response()
 
     app = web.Application()
     app.router.add_get('/', handler)
-    app.middlewares.append(XForwardedRelaxed())
+    _setup(app, XForwardedRelaxed())
     cl = await test_client(app)
-    resp = await cl.get('/')
-    assert resp.status == 200
+    resp = await cl.get('/', headers=[('X-Forwarded-For', '10.10.10.10'),
+                                      ('X-Forwarded-For', '20.20.20.20'),
+                                      ('X-Forwarded-Proto', 'https'),
+                                      ('X-Forwarded-Host', 'example.com')])
+    assert resp.status == 400
 
+
+async def test_x_forwarded_relaxed_multiple_proto(test_client):
+    async def handler(request):
+        return web.Response()
+
+    app = web.Application()
+    app.router.add_get('/', handler)
+    _setup(app, XForwardedRelaxed())
+    cl = await test_client(app)
+    resp = await cl.get('/', headers=[('X-Forwarded-For', '10.10.10.10'),
+                                      ('X-Forwarded-Proto', 'http'),
+                                      ('X-Forwarded-Proto', 'https'),
+                                      ('X-Forwarded-Host', 'example.com')])
+    assert resp.status == 400
+
+
+async def test_x_forwarded_relaxed_multiple_host(test_client):
+    async def handler(request):
+        return web.Response()
+
+    app = web.Application()
+    app.router.add_get('/', handler)
+    _setup(app, XForwardedRelaxed())
+    cl = await test_client(app)
+    resp = await cl.get('/', headers=[('X-Forwarded-For', '10.10.10.10'),
+                                      ('X-Forwarded-Proto', 'http'),
+                                      ('X-Forwarded-Host', 'example.org'),
+                                      ('X-Forwarded-Host', 'example.com')])
+    assert resp.status == 400
