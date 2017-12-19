0a76b94937caec0e42971e87336b7a3511bc94ec
Andrew Svetlov <andrew.svetlov@gmail.com>
2017-12-19 12:00:20 +0200

    Bump to 0.1.0

---
+++ CHANGES.rst
@@ -0,0 +1,4 @@
+2017-12-19
+----------
+
+* First public release
--- aiohttp_remotes/__init__.py
+++ aiohttp_remotes/__init__.py
@@ -6,7 +6,7 @@ aiohttp.web.Request if the server is deployed behind reverse proxy.
 """
 
 
-__version__ = '0.0.5'
+__version__ = '0.1.0'
 
 
 from .allowed_hosts import AllowedHosts
